ac425d54b55ad239978d4ea55d7d005fadcbd1df
Kjell Ivar Storstein <kjelle@gmail.com>
2014-04-25 00:34:52 +0200

    Fix nanoscroll bug, og resize

--- assets/modules/meetingroom/meetingroom.js
+++ assets/modules/meetingroom/meetingroom.js
@@ -1,10 +1,74 @@
 angular.module('meetingroom', ['sun.scrollable'])
-    .controller('meetingroomController', function($scope){
+    .controller('meetingroomController', function($scope, $window, $timeout){
 
-        var messages = [{
+        $scope.messages = [{
             user: "Andreas Ludvigsen",
             img: "http://placehold.it/60x60",
-            timestamp: new Date()
-        }]
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        },{
+            user: "Andreas Ludvigsen",
+            img: "http://placehold.it/60x60",
+            timestamp: new Date(),
+            text: ["This is a paragraph", "And this is another"]
+        }];
+
+        var msgBoxContainer = $("#messageBoxContainer");
+        var chatContainer = $("#chat");
+        var scrollChatContainer = $(".nano");
+
+
+        function initScroll() {
+            var toTop = chatContainer.offset().top;
+            var msgBox = msgBoxContainer.outerHeight();
+            var windowSize = angular.element($window).height();
+            var totalHeight = windowSize - msgBox - toTop;
+            scrollChatContainer.height(totalHeight);
+        }
+
+        angular.element( $window ).resize(function() {
+            initScroll();
+        });
+
+        $timeout(function(){
+            initScroll();
+        }, 100);
+
+
 
     });
