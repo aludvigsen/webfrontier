544e9c0c677ce89276d3a684adfaf5c541931f1f
aludvigsen <mailpostkasse@gmail.com>
2014-04-25 18:34:06 +0200

    Exchanged scroller plugin

--- assets/modules/meetingroom/meetingroom.js
+++ assets/modules/meetingroom/meetingroom.js
@@ -1,4 +1,17 @@
-angular.module('meetingroom', ['sun.scrollable'])
+angular.module('meetingroom', [])
+    .directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if(event.which === 13  && !event.shiftKey) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.ngEnter);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    })
     .controller('meetingroomController', function($scope, $window, $timeout){
 
         $scope.messages = [{
@@ -30,8 +43,9 @@ angular.module('meetingroom', ['sun.scrollable'])
             user: "Andreas Ludvigsen",
             img: "http://placehold.it/60x60",
             timestamp: new Date(),
-            text: ["This is a paragraph", "And this is another"]
-        },{
+            text: ["Check out this link http://www.vg.no", "&nbsp;", "And this is another"]
+        }
+        /*,{
             user: "Andreas Ludvigsen",
             img: "http://placehold.it/60x60",
             timestamp: new Date(),
@@ -46,19 +60,32 @@ angular.module('meetingroom', ['sun.scrollable'])
             img: "http://placehold.it/60x60",
             timestamp: new Date(),
             text: ["This is a paragraph", "And this is another"]
-        }];
+        }*/];
+
+        $scope.runThisMofo = function () {
+            var a = $("textarea").val().replace(/\n/g, "<br>");
+            $("textarea").val("");
+            console.log(a);
+        };
 
         var msgBoxContainer = $("#messageBoxContainer");
         var chatContainer = $("#chat");
-        var scrollChatContainer = $(".nano");
+        var scrollChatContainer = $("#myslimscroll");
 
 
         function initScroll() {
-            var toTop = chatContainer.offset().top;
+            var topbar = chatContainer.offset().top;
             var msgBox = msgBoxContainer.outerHeight();
             var windowSize = angular.element($window).height();
-            var totalHeight = windowSize - msgBox - toTop;
-            scrollChatContainer.height(totalHeight);
+            var totalHeight = windowSize - msgBox - topbar;
+            var width = scrollChatContainer.width();
+            scrollChatContainer.slimscroll({
+                width: width + 'px',
+                height: totalHeight + 'px',
+                //railVisible: true,
+                margin: 20,
+                start: 'bottom'
+            });
         }
 
         angular.element( $window ).resize(function() {
@@ -67,8 +94,11 @@ angular.module('meetingroom', ['sun.scrollable'])
 
         $timeout(function(){
             initScroll();
+
+            // Event handler for my awesome scroller
+            scrollChatContainer.slimScroll().bind('slimscroll', function(e, pos){
+                console.log("Reached " + pos);
+            });
         }, 100);
-
-
 
     });
